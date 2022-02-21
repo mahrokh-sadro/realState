d739267a5e64b1be4ff8ef5582017a0c54552423
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-21 00:58:23 -0500

    search.js

---
+++ pages/search.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import SearchFilters from "../components/SearchFilters";
+import Image from "next/image";
+import { Flex, Box, Text, Icon } from "@chakra-ui/react";
+import { BsFilter } from "react-icons/bs";
+
+const search = () => {
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box>
+      <Flex
+        cursor="pointer"
+        bg="gray.100"
+        borderBottom="1px"
+        borderColor="gray.200"
+        p="2"
+        fontWeight="black"
+        fontSize="lg"
+        justifyContent="center"
+        alignItems="center"
+        onClick={() => setSearchFilters(!searchFilters)}
+      >
+        <Text>Search Property By Filters</Text>
+        <Icon paddingLeft="2" w="7" as={BsFilter} />
+      </Flex>
+
+      {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" p="4" fontWeight="bold">
+        Properties {router.query.purpose}
+      </Text>
+      <Flex flexWrap="wrap"></Flex>
+    </Box>
+  );
+};
+
+export default search;
