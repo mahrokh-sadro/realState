806b91cca418e73f0c41e6be338f4c6f4d1ee7c9
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-21 19:05:19 -0500

    id.js

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -16,7 +16,17 @@ import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData);
-  const searchProperties = (filterValues) => {};
+  const router = useRouter();
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+    const values = getFilterValues(filterValues);
+    values.forEach((item) => {
+      query[item.name] = item.value;
+    });
+    router.push({ pathname: path, query: query });
+  };
   console.log(filters);
   return (
     <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
---
+++ pages/property/[id].js
@@ -0,0 +1,44 @@
+import { Box, Flex, Spacer, Text } from "@chakra-ui/layout";
+import { Avatar } from "@chakra-ui/avatar";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";
+
+import { baseUrl, fetchApi } from "../../utils/fetchApi";
+// import ImageScrollbar from "../../components/ImageScrollbar";
+
+import React from "react";
+
+const PropertyDetails = ({
+  propertyDetails: {
+    price,
+    rentFrequency,
+    rooms,
+    title,
+    baths,
+    area,
+    agency,
+    isVerified,
+    description,
+    type,
+    purpose,
+    furnishingStatus,
+    amenities,
+    photos,
+  },
+}) => {
+  return <div>PropertyDetails</div>;
+};
+
+export default PropertyDetails;
+
+export async function getServerSideProps({ params: { id } }) {
+  const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+
+  return {
+    props: {
+      propertyDetails: data,
+    },
+  };
+}
